fe80f8549b3940c970fb39bae3300a3b00cbf1f9
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-11-16 20:56:33 -0300

    Setup Cloudinary

--- src/routes/data_routes.rs
+++ src/routes/data_routes.rs
@@ -1,5 +1,7 @@
 use axum::{http::StatusCode, Json};
 use axum_typed_multipart::{FieldData, TryFromField, TryFromMultipart, TypedMultipart};
+use cloudinary::{upload::UploadOptions, Cloudinary, Source};
+use dotenvy::dotenv;
 use serde::Serialize;
 use std::{fs, path::Path};
 use tempfile::NamedTempFile;
@@ -52,18 +54,56 @@ pub struct UploadAssetRequest {
 pub async fn upload_asset(
     TypedMultipart(UploadAssetRequest { image, author }): TypedMultipart<UploadAssetRequest>,
 ) -> StatusCode {
+    dotenv().ok();
+
     let file_name = image.metadata.file_name.unwrap_or(String::from("data.bin"));
-    let user_path = Path::new("./tmp").join(author);
+    let user_path = Path::new("./tmp").join(&author);
 
     //Create directory
     fs::create_dir_all(&user_path).unwrap();
     let file_path = user_path.join(&file_name);
 
-    println!("{:?}", file_path);
-
-    // upload asset local
-    match image.contents.persist(file_path) {
+    // Upload asset local
+    match image.contents.persist(&file_path) {
         Ok(_) => StatusCode::CREATED,
         Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    };
+
+    // Cloudinary Credencials Setup
+    let api_key = dotenvy::var("CLOUDINARY_API_KEY").expect("enviroment variables not set");
+    let cloud_name = dotenvy::var("CLOUDINARY_CLOUD_NAME").expect("enviroment variables not set");
+    let api_secret = dotenvy::var("CLOUDINARY_API_SECRET").expect("enviroment variables not set");
+    let cloudinary = Cloudinary::new(api_key, cloud_name, api_secret);
+
+    // Remove the extension from the asset
+    let file_name_alone = Path::new(&file_name);
+    let public_name = file_name_alone
+        .file_stem()
+        .and_then(|stem| stem.to_str())
+        .unwrap_or_default();
+
+    //
+    let public_id = format!("{}/{}", &author, public_name);
+    let image_path = &file_path;
+
+    let options = UploadOptions::new()
+        .set_public_id(String::from(public_id))
+        .set_folder(String::from("storage"))
+        .set_overwrite(true);
+
+    let res = cloudinary
+        .upload_image(Source::Path(image_path.try_into().unwrap()), &options)
+        .await;
+
+    fs::remove_dir_all(&user_path).unwrap_or_else(|err| {
+        eprintln!("Error al eliminar contenido de la carpeta local: {:?}", err);
+    });
+
+    match res {
+        Ok(_) => StatusCode::CREATED,
+        Err(err) => {
+            eprintln!("Error uploading to Cloudinary: {:?}", err);
+            return StatusCode::INTERNAL_SERVER_ERROR;
+        }
     }
 }
